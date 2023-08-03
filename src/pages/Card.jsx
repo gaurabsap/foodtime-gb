edd170203158c96ad95378cbf382e63f8e8ddf00
Gaurab sapkota <iamgaurabsap20@gmail.com>
2023-08-03 11:18:37 +0545

    update home.jsx

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -7,8 +7,8 @@ const Card = ({ title }) => {
     setShow(!show);
   };
   return (
-    <div className="w-[70vw] lg:w-auto">
-      <div className="border border-gray-700 w-[100%] lg:w-[90%] py-2 px-5 flex flex-col gap-4 rounded-md">
+    <div className="w-[80vw] lg:w-auto">
+      <div className="border border-gray-700 w-full lg:w-[90%] py-5 px-5 flex flex-col gap-4 rounded-md">
         <div className="flex item-center justify-between w-full">
           <h1 className="font-semibold text-[14px] lg:text-[18px]">{title}</h1>
           <div onClick={Handletest1}>
